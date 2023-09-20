573b7386d11e60a898f926d50cb159a0f8eaa3e1
nhkhaidrive01@gmail.com <nhkhaidrive01@gmail.com>
2023-09-20 07:29:11 +0000

    Created LICENSE.txt; Edited Form1, Form2, ServerModule1, editor.css and 1 more

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form2Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 
 class Form2(Form2Template):
